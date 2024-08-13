36d640c7e5ed6dfd1a3045b1e2fe87b90dc83c26
Theo <thhHasABurgr@gmail.com>
2024-08-13 14:59:25 -0400

    fixed issues with hashtable

--- LexarANDparser/HashTable.h
+++ LexarANDparser/HashTable.h
@@ -3,7 +3,7 @@
 #ifndef HASHTABLE
 #define HASHTABLE
 
-unsigned long hash(unsigned char *str){
+unsigned int hash(unsigned char *str){
     unsigned long hash = 5381;
     int c;
 
@@ -14,13 +14,40 @@ unsigned long hash(unsigned char *str){
        
 }
 
-unsigned int HashTable[TABLESIZE];
 
-typedef struct{
-    const char* key;
+
+typedef struct Cell{
+    char* key;
     unsigned long val;
+    struct Cell* nextCell;
+
 }Cell;
-typedef struct{
-    
-}HashTable;
+
+
+void initCell(Cell* cell) {
+    cell->key = "";
+    cell->val = 0;
+    cell->nextCell=NULL;
+}
+// I think Cells should be an array of fixed size (prime num)
+// [NULL,2,NULL]
+
+int insert(Cell** cells, char* inp){
+    unsigned int index=hash((unsigned char*)inp);
+    Cell* exp= (Cell*)malloc(sizeof(Cell));
+    initCell(exp);
+    exp->key=inp;
+    exp->val=index;
+   
+    if(cells[index]==NULL){
+        cells[index]=exp;
+    }else{// cell alr has a pigeon in it
+        return -1;
+    }
+
+}
+int look(Cell** cells,char* keye){
+    return cells[hash((unsigned char*) keye)]->val;
+
+}
 #endif
